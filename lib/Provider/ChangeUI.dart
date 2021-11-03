3c07bd9b1fc607cd1dc0491758fcd3253a6f3b97
ZainChaudhary <zainabbas.386@gmail.com>
2021-11-03 09:48:47 +0500

    second Commit

--- lib/Provider/ChangeUI.dart
+++ lib/Provider/ChangeUI.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 
 class UIProvider extends ChangeNotifier {
   bool _isListView = true;
-
   bool get isListView => _isListView;
-
   toggleView() {
     _isListView = !_isListView;
     notifyListeners();
